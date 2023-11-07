8d2666e3f25a71f4741de9d4923f44036b688b44
Richard Mulholland <mulhollandrl@gmail.com>
2023-11-06 18:27:38 -0700

    Possibly done part 2 implementation

--- consumer.py
+++ consumer.py
@@ -9,6 +9,7 @@ logging.basicConfig(filename='consumer.log', level=logging.INFO)
 # This is work being done to parse the arguments for the storage strategy and resources to use as specified in Step 1
 parser = argparse.ArgumentParser(description='consumer for processing requests for widgets')
 parser.add_argument('--storage_strategy', type=str, help='Storage strategy to use.')
+parser.add_argument('--queue_name', type=str, help='Name of the SQS queue.')
 args = parser.parse_args()
 
 access_key = input("Please provide your access key: ")
@@ -24,53 +25,69 @@ session = boto3.Session(
     region_name='us-east-1a'
 )
 
-# Create the objects we need for s3 and dynamodb
+# Create the objects we need for s3, dynamodb and sqs
 s3 = session.resource('s3')
 dynamodb = session.resource('dynamodb')
+sqs = session.client('sqs')
 
-# Get all of the buckets we need and the dynamodb table
+# Get all of the buckets we need, the dynamodb table and the sqs queue
 bucket2 = s3.Bucket(bucket2_name)
 bucket3 = s3.Bucket(bucket3_name)
 table = dynamodb.Table(table_name)
+queue_url = sqs.get_queue_url(QueueName=args.queue_name)['QueueUrl']
 
-# Believe it or not, this processes the requests...
+# This processes the requests...
 def process_request(request):
-    # Right now, we only want to process create requests. We may change this later in other homework assignments
-    if request['type']['pattern'] == 'create':
+    # Now, we want to process create, delete and update requests.
+    if request['type']['pattern'] in ['create', 'delete', 'update']:
         # If the storage method is s3, then we need to make sure we parse it the correct way and then feed it in
         if args.storage_strategy == 's3':
             object_key = f"widgets/{request['owner']['pattern'].replace(" ", "-").lower()}/{request['widgetId']}"
             bucket3.put_object(Key=object_key, Body=json.dumps(request))
             # Log
-            logging.info(f'Widget created with id {request["widgetId"]}')
+            logging.info(f'Widget {request["type"]["pattern"]}d with id {request["widgetId"]}')
         # Meanwhile, DynamoDB will just accept anything haha
         elif args.storage_strategy == 'dynamodb':
-            table.put_item(Item=request)
+            if request['type']['pattern'] == 'create':
+                table.put_item(Item=request)
+            elif request['type']['pattern'] == 'delete':
+                table.delete_item(Key={'widgetId': request['widgetId']})
+            elif request['type']['pattern'] == 'update':
+                table.update_item(Key={'widgetId': request['widgetId']}, AttributeUpdates=request['updates'])
             # Log
-            logging.info(f'Widget created with id {request["widgetId"]}')
+            logging.info(f'Widget {request["type"]["pattern"]}d with id {request["widgetId"]}')
 
 # This is the bulk of the program, and the loop that you specified in the instructions
 def consumer_program():
+    # Cache for storing messages
+    message_cache = []
+
     while True:
-        # We want a list of all of the objects in the requests bucket
-        objects = list(bucket2.objects.all())
+        # If cache is empty, retrieve up to 10 messages from SQS queue
+        if not message_cache:
+            response = sqs.receive_message(
+                QueueUrl=queue_url,
+                AttributeNames=['All'],
+                MaxNumberOfMessages=10
+            )
 
-        # If there are actually objects in there, then we are good to continue
-        if objects:
-            # As per the instructions, we are sorting by the object keys, and then we are getting the key of the first object post-sort
-            objects.sort(key=lambda obj: obj.key)
-            object_key = objects[0].key
+            # If messages are found, add them to the cache
+            if 'Messages' in response:
+                message_cache.extend(response['Messages'])
 
-            # Get the object properties and then parse them with JSON per the requirements
-            body = bucket2.Object(object_key).get()['properties'].read().decode('utf-8')
-            request = json.loads(body)
+        # If there are messages in the cache, process them
+        if message_cache:
+            message = message_cache.pop(0)
+            request = json.loads(message['Body'])
 
-            # Delete the request
-            bucket2.Object(object_key).delete()
-
-            # And then process it
+            # Process the request
             process_request(request)
-        # If there are not, then we want to wait
+
+            # Delete the message from the queue
+            sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=message['ReceiptHandle']
+            )
         else:
             time.sleep(0.1)
 
